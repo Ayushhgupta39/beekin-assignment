712a076085635f41f4ebb2f1184fac61b94284c6
Ayush Gupta <ayushgupta3902@gmail.com>
2023-10-21 23:13:30 +0530

    chore: initial project setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import { Heading } from "@chakra-ui/react";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Heading>Job Board</Heading>
+    </div>
+  );
+}
+
+export default App;
