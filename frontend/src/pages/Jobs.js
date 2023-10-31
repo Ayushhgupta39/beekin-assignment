373fe13b93bda7fdff59ddc3c72d6716b4639a28
Ayush Gupta <ayushgupta3902@gmail.com>
2023-10-31 10:05:20 +0530

    feat: add loader while fetching jobs

--- frontend/src/pages/Jobs.js
+++ frontend/src/pages/Jobs.js
@@ -2,18 +2,23 @@ import React, { useEffect, useState } from "react";
 import { Navbar } from "../components";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, Spinner, Text } from "@chakra-ui/react";
 import JobCard from "../components/JobCard";
 
 const Jobs = () => {
   const [user, setUser] = useState(null);
   const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   const fetchJobs = async () => {
-    const response = await axios.get("https://beekin-job-board.onrender.com/jobs");
+    setLoading(true);
+    const response = await axios.get(
+      "https://beekin-job-board.onrender.com/jobs"
+    );
     console.log(response.data);
     setJobs(response.data);
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -39,21 +44,39 @@ const Jobs = () => {
         gap={"2"}
         rowGap={"4"}
       >
-        {jobs.map((job) => (
-          <JobCard
-            key={job._id}
-            id={job._id}
-            company={job.company}
-            description={job.description}
-            title={job.title}
-            experience={job.experience}
-            jobType={job.jobType}
-            location={job.location}
-            salary={job.salary}
-            benefits={job.benefits}
-            applicants={job.applicants}
-          />
-        ))}
+        {loading === false ? (
+          jobs.map((job) => (
+            <JobCard
+              key={job._id}
+              id={job._id}
+              company={job.company}
+              description={job.description}
+              title={job.title}
+              experience={job.experience}
+              jobType={job.jobType}
+              location={job.location}
+              salary={job.salary}
+              benefits={job.benefits}
+              applicants={job.applicants}
+            />
+          ))
+        ) : (
+          <Flex
+            alignItems={"center"}
+            justifyContent={"center"}
+            minHeight={"100vh"}
+            flexDirection={"column"}
+          >
+            <Spinner
+              thickness="4px"
+              speed="0.65s"
+              emptyColor="gray.200"
+              color="blue.500"
+              size="xl"
+            />
+            <Text fontSize={"2xl"}>Fetching jobs...</Text>
+          </Flex>
+        )}
       </Flex>
     </div>
   );
